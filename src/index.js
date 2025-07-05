23f12bd9ad28fe84836fc14f4500d29dbfb58934
Awwal <lambeawwal7@egmail.com>
2025-07-06 00:39:09 +0100

    Converted to PWA: fixed manifest and service worker

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// Register service worker
+serviceWorkerRegistration.register();
+
+// Optional: report web vitals
+reportWebVitals();
